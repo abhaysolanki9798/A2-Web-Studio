9582e76551f02c1e8da050660b17d8cacaca12d3
ABHAY SOLANKI <solankiabhay167@gmail.com>
2026-06-10 17:49:29 +0530

    update

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-
 const toggleBtn = document.querySelector(".toggle-btn");
 const navigation = document.querySelector(".navigation");
 
 toggleBtn.addEventListener("click", () => {
     navigation.classList.toggle("active");
+    toggleBtn.classList.toggle("active");
+
+    if (toggleBtn.classList.contains("active")) {
+        toggleBtn.innerHTML = "✖";
+    } else {
+        toggleBtn.innerHTML = "☰";
+    }
 });
